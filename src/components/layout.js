6bc9bdf087e42138b884661173f2694c7dbe58e4
이강호 <cannalee90@gmail.com>
2019-02-07 00:31:12 +0900

    스포카 한 산스 적용

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,7 +1,7 @@
-import React from "react"
-import { Link } from "gatsby"
-
-import { rhythm, scale } from "../utils/typography"
+import React from "react";
+import { Link } from "gatsby";
+import { rhythm, scale } from "../utils/typography";
+import '../assets/spoqa-han-sans-kr.css';
 
 class Layout extends React.Component {
   render() {
