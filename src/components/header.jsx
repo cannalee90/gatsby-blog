764be64fc94b0c8770b7c61bbdcdc419f3c6a3c7
이강호 <kangho@bluehole.net>
2019-02-16 23:57:46 +0900

    header position 수정

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -5,7 +5,7 @@ import styled from "styled-components";
 const Container = styled.div`
   border-bottom: 1px solid #e6e6e6;
   background-color: #fff;
-  position: fixed;
+  position: absolute;
   display: flex;
   width: 100%;
   height: 60px;
